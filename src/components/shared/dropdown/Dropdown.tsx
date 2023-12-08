fa1b671e9c02cc97c920b752c0df628d205b04f3
mook2005 <chananthorn.s@gmail.com>
2023-12-09 05:18:37 +0700

    styledropdown

--- src/components/shared/dropdown/Dropdown.tsx
+++ src/components/shared/dropdown/Dropdown.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useCategoryMajorContext } from "../../../context/category/CategoryMajorContext";
+import { useColorContext } from "../../../context/ColorContext";
+import "./Dropdown.css";
 
 type DropdownProps = {
   name: string;
@@ -10,6 +12,7 @@ type DropdownProps = {
 };
 
 const Dropdown = (props: DropdownProps) => {
+  const { color } = useColorContext();
   const [isOpen, setIsOpen] = useState(false);
   const { setCategoryMajor } = useCategoryMajorContext();
 
@@ -35,46 +38,38 @@ const Dropdown = (props: DropdownProps) => {
         <button
           type="button"
           onClick={toggleDropdown}
-          className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-none"
+          className="flex flex-col gap-[1px] justify-center items-start bg-white/70 w-60 rounded-2xl px-3 py-1 h-[50px]"
           id="menu-button"
           aria-expanded={isOpen}
           aria-haspopup="true"
         >
-          {`${props.name}: ${props.selected}`}
-          <svg
-            className={`-mr-1 h-5 w-5 text-gray-400 ${
-              isOpen ? "transform rotate-180" : ""
-            }`}
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <span className={`text-xs ${color}-text-secondary`}>{props.name} :</span>
+          <span className={`text-xl flex justify-between items-center w-full px-2 text-${color} fill-${color}`}>
+            <span>{props.selected}</span>
+            <svg height="16" width="16" viewBox="0 0 512 512">
+              <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
+            </svg>
+          </span>
         </button>
       </div>
 
       {isOpen && (
         <div
           id="dropdown-content"
-          className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+          className="absolute right-0 z-10 mt-2 w-56 rounded-2xl origin-top-right bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
           role="menu"
           aria-orientation="vertical"
           aria-labelledby="menu-button"
           tabIndex={-1}
         >
-          <div className="py-1" role="none">
+          <div className="" role="none">
             {/* Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" */}
 
             {props.selectList.map((v, index) => (
               <button
                 onClick={() => selectHandler(v)}
                 key={index}
-                className={`w-full text-start text-gray-700 block px-4 py-2 text-sm hover:bg-gray-200 ${
+                className={`w-full text-start text-${color} block px-4 py-2 text-sm rounded-xl hover:bg-gray-200 ${
                   props.selected === v ? "bg-gray-300" : ""
                 }`}
                 role="menuitem"
